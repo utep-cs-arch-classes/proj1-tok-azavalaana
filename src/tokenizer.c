2682a0ad07a7c7bd5e42e8f98428699da72d8f38
Antonio Zavala <azavalaana@miners.utep.edu>
2021-02-11 19:48:00 -0700

    added copy string method helper

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -3,6 +3,15 @@
 #include <stdlib.h>
 #include "input.c"
 
+/* Copy_str helper method to know the length of the string <len> */
+int string_length(char* str){
+  int index = 0;
+  long count = 0;
+  while(str[index++]){
+    count++;
+  }
+  return count;
+}
 /* Returns a fresly allocated new string containing <len> chars from <inStr> */
 char* copy_str(char* inStr, short len){
     char* copy = (char*)malloc(len+1 * sizeof(char));
