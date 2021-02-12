e8bafdbd927281ba5935416e0c0a85ec6ac62f10
Antonio Zavala <azavalaana@miners.utep.edu>
2021-02-11 19:47:16 -0700

    copy string

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#include "tokenizer.h"
+#include <stdlib.h>
+#include "input.c"
+
+/* Returns a fresly allocated new string containing <len> chars from <inStr> */
+char* copy_str(char* inStr, short len){
+    char* copy = (char*)malloc(len+1 * sizeof(char));
+    int i = 0;
+    for(i; i<len-1; i++) {
+        //printf("[%d]: %c", i, inStr[i], "\n");
+        copy[i] = inStr[i];
+        //printf("[%d]: %c", i, copy[i], "\n");
+    }
+    return copy;
+}
 
 int main()
 {
-  printf("hello world\n");
-  return 0;
+    int loop = 1;
+    
+    while (loop == 1) {
+        char* test = input();
+        int len = string_length(test);
+        printf("Copy: %s\n ", copy_str(test, len));
+    }
 }
