0b6cb4af1b97b9644547c90117f14424ef278576
Antonio Zavala <azavalaana@miners.utep.edu>
2021-02-15 18:37:22 -0700

    update fix in count words

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -47,6 +47,10 @@ int count_words(char* str){
     int i = 0;
     int count = 0;
 
+    if(str[0] == ' '){
+      return 0;
+    }
+
     while(str[i] != '\0'){
         if(str[i] == ' ' && str[i+1] != ' '){
             count++;
